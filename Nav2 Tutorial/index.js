346c683a5c4fe0f3c8baf2c12a7aa88d871992b1
Abhishek Kumar <abhikumar21@iitk.ac.in>
2023-06-19 18:25:20 +0530

    Sliding_Navbar

---
+++ Nav2 Tutorial/index.js
@@ -0,0 +1,12 @@
+
+const toggle = document.querySelector('.menu_toggle');
+const menu = document.querySelector('.menu');
+
+const show_menu = () => {
+    toggle.addEventListener('click', ()=> {
+        toggle.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+}
+
+show_menu();
